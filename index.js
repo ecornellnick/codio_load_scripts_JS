86ea7bd457f1c0b8e10151e2c25c00b1c2068938
Nick <44708582+ecornellnick@users.noreply.github.com>
2026-02-11 13:38:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,7 @@
   const scripts = [
     "https://code.jquery.com/jquery-3.4.1.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
-    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
-    "js/popper.js"
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
   ];
 
   scripts.forEach(src => {
