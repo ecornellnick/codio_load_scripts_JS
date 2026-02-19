9f5b051108c1196803360a2507acb86be719a4cb
Nick <44708582+ecornellnick@users.noreply.github.com>
2026-02-19 10:53:02 -0500

    Update script sources to newer versions

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
   // Order matters due to dependencies:
   // jQuery → Popper → Bootstrap → local overrides
   const scripts = [
-    "https://code.jquery.com/jquery-3.4.1.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
-    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+    "https://code.jquery.com/jquery-3.7.1.min.js,
+    "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.6.2/js/bootstrap.min.js"",
     "https://cdn.jsdelivr.net/gh/ecornellnick/codio_load_scripts_JS@main/utilities/utilities.js"
   ];
 
